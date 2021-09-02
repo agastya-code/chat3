0792ba0f1f787d725ef0d64de90a1c2479ad14b3
asimo3089 <79460561+agastya-code@users.noreply.github.com>
2021-09-02 22:12:41 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,12 @@
+function addroom()
+{
+    room_name = document.getElementById("room_name").value;
+
+    firebase.database().ref("/").child(room_name).update({
+       purpose : "adding room name" 
+    });
+
+ localStorage.setItem("room_name", room_name);
+
+ window.location = "kwitter_page.html";
+}
